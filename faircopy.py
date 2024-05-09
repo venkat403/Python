7c77c8f2e182c1a107b45b05e378210699271ccf
venkat403 <120652924+venkat403@users.noreply.github.com>
2024-05-09 19:53:33 +0530

    Add files via upload

---
+++ faircopy.py
@@ -0,0 +1,56 @@
+class faircopy():
+    def SubfieldsInAI():
+        Learn=["Machine Learning", "Neural Networks", "Vision", "Robotics", "Speech Processing", "Natural Language Processing"] 
+        for a in Learn:
+            print(a)
+            value="a"
+        return a
+    def OddEven():
+        num1=int(input("Enter a number: "))
+        if(num1%2)==0:
+            print(num1,"is Even number")
+        else:
+            print(num1,"is Odd number")
+        return num1
+    def  ElegiblityForMarriage():
+        gender=str(input("Your Gender: "))
+        age=int(input("Your Age: "))
+        if(age<23):
+            print("NOT ELIGIBLE")
+            Elegible="NOT ELIGIBLE"
+        else:
+            print("ELIGIBLE")
+            Elegible="ELIGIBLE"
+        return Elegible
+    def percentage():
+        Sub1= int(input("Subject1= "))
+        Sub2= int(input("Subject2= "))
+        Sub3= int(input("Subject3= "))
+        Sub4= int(input("Subject4= "))
+        Sub5= int(input("Subject5= "))
+        add=Sub1+Sub2+Sub3+Sub4+Sub5
+        print("Total :",add)
+        final=add
+        Percent=add/5
+        print("Percentage:",Percent)
+        final=Percent
+        return final
+    def triangle():
+        h1=int(input("Height:"))
+        b1=int(input("Breadth:"))
+        Area_formula=h1*b1/2
+        print("Area formula:", Area_formula)
+        formula=Area_formula
+        h2=int(input("Height1: "))
+        h3=int(input("Height2: "))
+        b2=int(input("Breadth: "))
+        print("Perimeter formula: Height1+Height2+Breadth")
+        f1=h2+h3+b2
+        print("Perimeter of Triangle:",f1)
+        formula=f1
+        return formula
+
+
+
+
+
